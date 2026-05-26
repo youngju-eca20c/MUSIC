ac4ae527989f7a0f215ef384d51addde61ce8c9e
AlgoVision <algolord97@gmail.com>
2026-05-26 11:09:37 +0900

    Crossfade: 900ms → 500ms

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const audios = [$('audio'), $('audio-b')];
 let activeAudioIdx = 0;
 const activeAudio = () => audios[activeAudioIdx];
 const otherAudio = () => audios[1 - activeAudioIdx];
-const CROSSFADE_MS = 900;
+const CROSSFADE_MS = 500;
 let fadeFrame = null;
 
 const artCard = $('art-card');
